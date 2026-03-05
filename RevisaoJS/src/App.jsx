2dab4901f38b9e8887f66fae3410dc0ff29c7dfc
Daves Martins <davesmartins@daves.com>
2026-03-04 22:18:22 -0300

    Aula 04-03

--- RevisaoJS/src/App.jsx
+++ RevisaoJS/src/App.jsx
@@ -13,6 +13,29 @@ function App() {
     "Gustin"
   ]
 
+  let vAlunos2 = [
+    {id: 1, nome: "PEdrin", nota: 8},
+    {id: 2, nome: "JEfin", nota: 9},
+    {id: 3, nome: "Zezin", nota: 4}
+  ]
+
+  vAlunos2.push({id: 4, nome: "Marquin", nota: 6})
+
+  let vAlunosAprovados = vAlunos2.filter( (aluno, idx)=> aluno.nota >= 6 )
+
+  let alu = {id: 3, nome: "Gustin", nota: 10, 
+    disciplina: [ {nome: "matematica", prof:"Martin"},
+                  {nome: "Quimica", prof:"Isa"}
+    ]}
+
+  console.log(alu.disciplina[0].nome)
+  console.log(alu.nome)
+
+  console.log(vAlunos2[0])
+  console.log(vAlunos2[0].nome)
+  console.log(vAlunos2[0].nota)
+
+
   // function x(){
   //   return "OLÁ"
   // }
@@ -34,10 +57,36 @@ function App() {
   //   console.log(`Função Anônima:: Olá ${v} seu indice é ${i}`)
   // )
 
+  let imprimir = (mensagem)=>{
+      // let nome = mensagem.nome
+      // let idade = mensagem.idade
+
+      let {nome, idade} = mensagem
+      let msg1 = {peso: 50, mensagem}
+      let msg2 = {peso: 50, ...mensagem}
+      console.log(nome, idade)
+      console.log(msg1)
+      console.log(msg2)
+  }
+
+ // imprimir("Daves MArtins")
+  imprimir( {nome: "Daves", idade: 18} )
+
+
   return (
     <>
      <div>
       <h1>Olá Mundo, {vNome} - {vMaior}</h1>
+    <h2>Lista de Alunos 2</h2>
+    <ul>
+      {vAlunos2.map(  (aluno, idx) => <li>{aluno.nome} - nota {aluno.nota}</li> )}
+      {/* <li>Zezin - nota 10</li>
+      <li>Mariazinha - nota 10</li> */}
+    </ul>
+
+    <h2>Lista de Alunos 2 Aprovados</h2>
+  {vAlunosAprovados.map(  (aluno, idx) => <li>{aluno.nome} - nota {aluno.nota}</li> )}
+     
       <h2>Lista de Alunos</h2>
       <ul>
         {vAlunos.map( (v, i) => <li>{v} - {i}</li> )}
