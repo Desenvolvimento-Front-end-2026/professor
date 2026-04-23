297957b30e88f0ba3d7a0b3f10c77a08fb92348a
Daves Martins <davesmartins@daves.com>
2026-04-22 22:30:17 -0300

    aula 22-04 - resolvendo problema da atualização

--- portifolio/src/Componentes/Context/UserContext.jsx
+++ portifolio/src/Componentes/Context/UserContext.jsx
@@ -7,14 +7,54 @@ const UserAuth = ()=>{
     return useContext(AuthContext)
 }
 
+
 const AuthProvider = ({children}) =>{
+
+const SECRET_KEY = "Qualquer Chave ABC"
+const encryptData = (text) => {
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+        result += String.fromCharCode(text.charCodeAt(i) ^ SECRET_KEY.charCodeAt(i % SECRET_KEY.length));
+    }
+
+    return btoa(result);
+}
+
+const decryptData = (encodedText) => {
+    try {
+        const text = atob(encodedText);
+        let result = "";
+        for (let i = 0; i < text.length; i++) {
+            result += String.fromCharCode(text.charCodeAt(i) ^ SECRET_KEY.charCodeAt(i % SECRET_KEY.length));
+        }
+        return result;
+    } catch (error) {
+        return null; // Caso os dados estejam corrompidos ou alterados manualmente
+    }
+}
+
     let userList = [
         {nome: "Zezin da Silva", login: "ze", senha: "123", role:"USER"},
         {nome: "Pedrin Augusto", login: "ped", senha: "123", role:"USER"},
         {nome: "Gustin Carrara", login: "gugu", senha: "123", role:"ADMIN"},
     ]
-
-    const [userLogado, setUserLogado] = useState(null);
+''
+    const [userLogado, setUserLogado] = useState(()=>{
+        // const user =  JSON.parse(localStorage.getItem("logado")) 
+        // console.log("logado :: ",user)
+        // if (!user){
+        //     return null
+        // }else{
+        //     return user
+        // }
+        const login = decryptData(localStorage.getItem("logado")) 
+        console.log("logado :: ",login, localStorage.getItem("logado"))
+        if (!login){
+            return null
+        }else{
+            return userList.filter( u => u.login === login)[0]
+        }
+    });
     const versao = "1.0.0"
 
     const login = (login, senha) =>{
@@ -22,6 +62,8 @@ const AuthProvider = ({children}) =>{
 
         //console.log("Login::",user)
         if (user.length > 0){
+            // localStorage.setItem("logado", JSON.stringify(user[0]) )
+            localStorage.setItem("logado",  encryptData(user[0].login)  )
             setUserLogado(user[0])
         }else{
             setUserLogado(null)
